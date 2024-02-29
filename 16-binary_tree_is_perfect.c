b50c09ed353a4aceeb09463bfd01456d45d11eb9
Ebenie <abrahamavenue163@.com>
2024-02-29 11:40:50 -0800

    i think it is fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -16,16 +16,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-
 	if (left_height != right_height)
 		return (0);
 
-
 	if (binary_tree_is_full(tree) && binary_tree_is_perfect(tree->left)
-		&& binary_tree_is_perfect(tree->right))
+	    && binary_tree_is_perfect(tree->right))
 		return (1);
 
 	return (0);
 }
-
 
