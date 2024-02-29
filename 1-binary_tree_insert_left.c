13c69bf4864c20122484c990b520dd56edfa5cea
Ebenie <abrahamavenue163@.com>
2024-02-28 23:16:47 -0800

    the binary tree project solutions

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,21 +2,22 @@
 
 /**
  * binary_tree_insert_left - Inserts a node as the left-child of another node
- * @parent: A pointer to the node to insert the left-child in
+ * @parent: pointer to the node to insert the left-child in
  * @value: The value to store in the new node
  *
- * Return: A pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer of created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
 
     if (parent == NULL)
-        return NULL;
+        return (NULL);
 
     new_node = malloc(sizeof(binary_tree_t));
+
     if (new_node == NULL)
-        return NULL;
+        return (NULL);
 
 
     new_node->n = value;
@@ -36,5 +37,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
     parent->left = new_node;
 
-    return new_node;
+    return (new_node);
 }
