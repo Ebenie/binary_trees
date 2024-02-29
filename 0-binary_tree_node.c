6afeaf304a1481490584697a3081f9aca4381937
Ebenie <abrahamavenue163@.com>
2024-02-28 23:08:21 -0800

    the binary tree project solutions

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node
+ * binary_tree_node - in short creates a binary tree node
  *
  * @parent: Pointer to the parent node of the node to create
  * @value: Value to put in the new node
@@ -11,6 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
