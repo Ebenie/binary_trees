2300880f9a8a1e6f5d5afed6620a41f3b3fe47eb
Ebenie <abrahamavenue163@.com>
2024-02-28 23:54:29 -0800

    The binary tree project solutions

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,19 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: A pointer to the root node of the tree to delete
+ * binary_tree_is_leaf - Checks if a node is a leaf or not
  *
- * freeing each node starting from the leaves to the root.
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise
  */
-void binary_tree_delete(binary_tree_t *tree)
+
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (tree == NULL)
-		return;
+    if (node == NULL) {
+        return 0;
+    }
 
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
 
-	free(tree);
+    if (node->left == NULL && node->right == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
