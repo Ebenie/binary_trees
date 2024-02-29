2eb84b94632dc634745d100484eb6e17bfdc787c
Ebenie <abrahamavenue163@.com>
2024-02-29 01:22:25 -0800

    binary tree solutions

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,6 +2,7 @@
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ *
  * @first: A pointer to the first node
  * @second: A pointer to the second node
  *
@@ -9,50 +10,45 @@
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    const binary_tree_t *ptr1 = first, *ptr2 = second;
-    size_t depth1 = 0, depth2 = 0;
+	const binary_tree_t *ptr1 = first, *ptr2 = second;
+	size_t depth1 = 0, depth2 = 0;
 
-    if (first == NULL || second == NULL)
-        return NULL;
+	if (first == NULL || second == NULL)
+		return NULL;
 
+	while (ptr1->parent != NULL)
+	{
+		ptr1 = ptr1->parent;
+		depth1++;
+	}
 
-    while (ptr1->parent != NULL)
-    {
-        ptr1 = ptr1->parent;
-        depth1++;
-    }
+	while (ptr2->parent != NULL)
+	{
+		ptr2 = ptr2->parent;
+		depth2++;
+	}
 
+	ptr1 = first;
+	ptr2 = second;
 
-    while (ptr2->parent != NULL)
-    {
-        ptr2 = ptr2->parent;
-        depth2++;
-    }
+	while (depth1 > depth2)
+	{
+		ptr1 = ptr1->parent;
+		depth1--;
+	}
 
+	while (depth2 > depth1)
+	{
+		ptr2 = ptr2->parent;
+		depth2--;
+	}
 
-    ptr1 = first;
-    ptr2 = second;
+	while (ptr1 != ptr2)
+	{
+		ptr1 = ptr1->parent;
+		ptr2 = ptr2->parent;
+	}
 
-
-    while (depth1 > depth2)
-    {
-        ptr1 = ptr1->parent;
-        depth1--;
-    }
-
-    while (depth2 > depth1)
-    {
-        ptr2 = ptr2->parent;
-        depth2--;
-    }
-
-
-    while (ptr1 != ptr2)
-    {
-        ptr1 = ptr1->parent;
-        ptr2 = ptr2->parent;
-    }
-
-    return (binary_tree_t *)ptr1;
+	return (binary_tree_t *)ptr1;
 }
 
