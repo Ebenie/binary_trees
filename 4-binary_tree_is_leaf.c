aab776e8030a79d138f4c813dbf0d35864bd0342
Ebenie <abrahamavenue163@.com>
2024-02-29 00:01:14 -0800

    binay tree solutions

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,18 +7,18 @@
  *
  * Return: 1 if the node is a leaf, 0 otherwise
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL) {
-        return 0;
-    }
+	if (node == NULL)
+		return (0);
 
-
-    if (node->left == NULL && node->right == NULL) {
-        return 1;
-    } else {
-        return 0;
-    }
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
--- a.c
+++ a.c
@@ -13,8 +13,12 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 
 	if (node->left == NULL && node->right == NULL)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
 
