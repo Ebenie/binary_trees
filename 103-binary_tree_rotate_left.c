bbab621cce0a5e274789df2b17ffcea3938da5cb
Ebenie <abrahamavenue163@.com>
2024-03-01 04:49:18 -0800

    binary tree updated

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,19 +8,19 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+    binary_tree_t *new_root = NULL;
+
     if (tree == NULL || tree->right == NULL)
-        return tree;
+        return (NULL);
 
-    binary_tree_t *new_root = tree->right;
+    new_root = tree->right;
     tree->right = new_root->left;
-
     if (new_root->left != NULL)
         new_root->left->parent = tree;
-
     new_root->left = tree;
     new_root->parent = tree->parent;
     tree->parent = new_root;
 
-    return new_root;
+    return (new_root);
 }
 
