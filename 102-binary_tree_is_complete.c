9b20e0ff1a7e6f8c50525046a5cad219f139ac6a
Ebenie <abrahamavenue163@.com>
2024-03-01 04:44:06 -0800

    binary tree updated

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 
-
+int is_complete(const binary_tree_t *tree, int index, int nodes);
+int binary_tree_is_complete(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete
  * @tree: A pointer to the root node of the tree to check
@@ -9,58 +11,51 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+    int nodes = 0, index = 0;
+
     if (tree == NULL)
-        return 0;
+        return (0);
 
 
-    binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 10000);
-    if (queue == NULL)
-        return 0;
+    nodes = binary_tree_size(tree);
 
-    int front = 0, rear = 0;
-    int flag = 0;
+    return (is_complete(tree, index, nodes));
+}
 
-    queue[rear++] = (binary_tree_t *)tree;
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ *
+ * Return: The size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
 
-    while (front < rear)
-    {
-        const binary_tree_t *current = queue[front++];
+    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * is_complete - Recursive helper function to check if a binary tree is complete
+ * @tree: A pointer to the current node in the tree
+ * @index: The index of the current node
+ * @nodes: The total number of nodes in the tree
+ *
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int is_complete(const binary_tree_t *tree, int index, int nodes)
+{
+
+    if (tree == NULL)
+        return (1);
 
 
-        if (flag && (current->left != NULL || current->right != NULL))
-        {
-            free(queue);
-            return 0;
-        }
+    if (index >= nodes)
+        return (0);
 
 
-        if (current->left == NULL)
-            flag = 1;
-
-        else if (flag)
-        {
-            free(queue);
-            return 0;
-        }
-
-        if (current->left != NULL)
-            queue[rear++] = current->left;
-
-
-        if (current->right == NULL)
-            flag = 1;
-
-        else if (flag)
-        {
-            free(queue);
-            return 0;
-        }
-
-        if (current->right != NULL)
-            queue[rear++] = current->right;
-    }
-
-    free(queue);
-    return 1;
+    return (is_complete(tree->left, 2 * index + 1, nodes) &&
+            is_complete(tree->right, 2 * index + 2, nodes));
 }
 
