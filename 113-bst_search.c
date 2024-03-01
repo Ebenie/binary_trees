dde7b12c60f806172e412b18a18e01fbd973d727
Ebenie <abrahamavenue163@.com>
2024-03-01 05:08:09 -0800

    binary tree updated

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -5,24 +5,21 @@
  * @tree: A pointer to the root node of the BST to search
  * @value: The value to search in the tree
  *
- * Return: A pointer to the node containing a value equals to value,
- *         or NULL if tree is NULL or if nothing is found
+ * Return: A pointer to the node containing the value, or NULL if not found
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
     if (tree == NULL)
         return NULL;
 
-    const bst_t *current = tree;
-
-    while (current != NULL)
+    while (tree != NULL)
     {
-        if (current->n == value)
-            return (bst_t *)current;
-        else if (value < current->n)
-            current = current->left;
+        if (value == tree->n)
+            return (bst_t *)tree;
+        else if (value < tree->n)
+            tree = tree->left;
         else
-            current = current->right;
+            tree = tree->right;
     }
 
     return NULL;
